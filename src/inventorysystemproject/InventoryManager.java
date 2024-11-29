71283c652ee9b00c40f28f9a159f73fae7303169
carl-josiah <carl_josiah@yahoo.com>
2024-11-30 01:07:11 +0400

    its late at night

--- src/inventorysystemproject/InventoryManager.java
+++ src/inventorysystemproject/InventoryManager.java
@@ -33,10 +33,6 @@ public class InventoryManager
     public void removeItem(String id)
     {
         InventoryItem removedItem = items.remove(id);
-        if (removedItem != null)
-            System.out.println("Item with ID " + id + " has been removed.");
-        else
-            System.out.println("Item not found.");
     }
 
     public InventoryItem findItemById(String id)
@@ -55,19 +51,21 @@ public class InventoryManager
 
         System.out.println(String.format("%-10s %-20s %-15s %-10s %-10s %-30s",
                 "ID", "Product Name", "Category", "Quantity", "Price", "Suppliers"));
+        System.out.println("----------------------------------------------------------------------------------------------------------" +
+                "------------------------------------------------------");
         while (keyIterator.hasNext())
         {
             String key = keyIterator.next();
             InventoryItem item = items.getValue(key);
 
-            System.out.println(String.format("%-10s %-20s %-15s %-10d %-10.2f %-30s",
+            System.out.printf("%-10s %-20s %-15s %-10d %-10.2f %-30s%n",
                     item.getId(),
                     item.getName(),
                     item.getCategory(),
                     item.getStockQuantity(),
                     item.getPrice(),
                     item.getSuppliers()
-            ));
+            );
         }
     }
 
@@ -80,6 +78,8 @@ public class InventoryManager
     {
         InventoryItem item1 = new InventoryItem("ID-101", "Name", "Category", 1, 10.0);
         item1.addSuppliers("Mister");
+        item1.addSuppliers("Mister1");
+        item1.addSuppliers("Mister2");
         System.out.println(item1.getSuppliers());
         InventoryManager inventoryManager = new InventoryManager("src/inventorysystemproject/item_inventory.csv");
         inventoryManager.addItem(item1);
